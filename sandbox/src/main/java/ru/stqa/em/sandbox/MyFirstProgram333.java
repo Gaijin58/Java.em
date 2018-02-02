3f18086f81c241a8a495c58009d4f196cf2ba4c3
Gaijin58 <36069016+Gaijin58@users.noreply.github.com>
2018-02-02 15:38:46 +0300

    Создан пакет для мойе первой программы

--- sandbox/src/main/java/ru/stqa/em/sandbox/MyFirstProgram333.java
+++ sandbox/src/main/java/ru/stqa/em/sandbox/MyFirstProgram333.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram {
+package ru.stqa.em.sandbox;
+
+public class MyFirstProgram333 {
 
  public static void main(String[] args) {
       System.out.println("Hello, world!");
